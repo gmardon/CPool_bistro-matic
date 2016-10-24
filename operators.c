ab92c034b84279625ebb0b939e2be7e9cbfb7fd3
Guillaume MARDON <guillaume.mardon@epitech.eu>
2016-10-24 10:18:54 +0200

    add somes

--- operators.c
+++ operators.c
@@ -5,21 +5,29 @@
 ** Login   <guillaume.mardon@epitech.eu>
 **
 ** Started on  Mon Oct 24 10:02:37 2016 Guillaume MARDON
-** Last update Mon Oct 24 10:12:37 2016 Guillaume MARDON
+** Last update Mon Oct 24 10:18:39 2016 Guillaume MARDON
 */
 #include <stdio.h>
+#include <stdlib.h>
 #include "my_strlen.c"
 
 char	*multiply_inf(char *val1, char *val2)
 {
   int	indexVal1;
   int 	indexVal2;
+  char	*result;
 
+  result = malloc(2048);
+
+  indexVal1 = 0;
   while (indexVal1 < my_strlen(val1))
     {
+      printf("%d x %d", val1[indexVal1], val2[indexVal2]);
+      indexVal2 = 0;
       while (indexVal2 < my_strlen(val2))
 	{
-
+	  indexVal2++;
 	}
+      indexVal1++;
     }
 }
