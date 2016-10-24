0128cb4a229efa0f0b5900adeed4e8b2991022de
Guillaume MARDON <guillaume.mardon@epitech.eu>
2016-10-24 10:12:45 +0200

    somes changes

--- operators.c
+++ operators.c
@@ -5,9 +5,21 @@
 ** Login   <guillaume.mardon@epitech.eu>
 **
 ** Started on  Mon Oct 24 10:02:37 2016 Guillaume MARDON
-** Last update Mon Oct 24 10:03:16 2016 Guillaume MARDON
+** Last update Mon Oct 24 10:12:37 2016 Guillaume MARDON
 */
+#include <stdio.h>
+#include "my_strlen.c"
+
 char	*multiply_inf(char *val1, char *val2)
 {
+  int	indexVal1;
+  int 	indexVal2;
 
+  while (indexVal1 < my_strlen(val1))
+    {
+      while (indexVal2 < my_strlen(val2))
+	{
+
+	}
+    }
 }
