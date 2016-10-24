7d3e05712f9a4465ef6e131e9ca89c2de90c0e0c
Guillaume MARDON <guillaume.mardon@epitech.eu>
2016-10-24 10:48:04 +0200

    add somes

--- operators.c
+++ operators.c
@@ -5,7 +5,7 @@
 ** Login   <guillaume.mardon@epitech.eu>
 **
 ** Started on  Mon Oct 24 10:02:37 2016 Guillaume MARDON
-** Last update Mon Oct 24 10:40:40 2016 Guillaume MARDON
+** Last update Mon Oct 24 10:47:49 2016 Guillaume MARDON
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,7 +17,7 @@ char	*multiply_inf(char *val1, char *val2)
   int 	indexVal2;
   char	*result;
 
-  result = malloc(2048);
+  result = malloc(sizeof(char*) * (my_strlen(val1) + my_strlen(val2)));
 
   indexVal1 = 0;
   while (indexVal1 < my_strlen(val1))
@@ -26,7 +26,7 @@ char	*multiply_inf(char *val1, char *val2)
       while (indexVal2 > 0)
 	{
 	  indexVal2--;
-	  printf("%d x %d = %d\n", (val1[indexVal1] - 48), (val2[indexVal2] - 48), (val1[indexVal1] - 48) * (val2[indexVal2] - 48
+	  printf("%d x %d = %d\n", (val1[indexVal1] - 48), (val2[indexVal2] - 48), (val1[indexVal1] - 48) * (val2[indexVal2] - 48));
 	}
       // ADD ONE 0
       indexVal1++;
@@ -35,5 +35,10 @@ char	*multiply_inf(char *val1, char *val2)
 
 char	*add_inf(char *val1, char *val2)
 {
+  int	indexVal1;
+  int 	indexVal2;
+  char	*result;
+
+  result = malloc(sizeof(char*) * ((my_strlen(val2) < my_strlen(val1) ? my_strlen(val1) : my_strlen(val2)) + 1));
 
 }
