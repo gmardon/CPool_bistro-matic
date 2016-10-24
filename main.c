5f3940615e1efdbc627917ccb1accd1400b523b8
Guillaume MARDON <guillaume.mardon@epitech.eu>
2016-10-24 12:45:06 +0200

    fix somes

--- main.c
+++ main.c
@@ -5,11 +5,11 @@
 ** Login   <guillaume.mardon@epitech.eu>
 **
 ** Started on  Mon Oct 24 10:03:22 2016 Guillaume MARDON
-** Last update Mon Oct 24 11:14:10 2016 Guillaume MARDON
+** Last update Mon Oct 24 12:44:23 2016 Guillaume MARDON
 */
 #include "operators.c"
 
 int	main(int argc, char *argv)
 {
-  add_inf("1234567897654567845678909045", "1234567897654567845678909045");
+  printf("456789098765478 + 93749026462893 = %s", add_inf("456789098765478", "93749026462893"));
 }
--- operators.c
+++ operators.c
@@ -5,7 +5,7 @@
 ** Login   <guillaume.mardon@epitech.eu>
 **
 ** Started on  Mon Oct 24 10:02:37 2016 Guillaume MARDON
-** Last update Mon Oct 24 11:18:25 2016 Guillaume MARDON
+** Last update Mon Oct 24 12:42:34 2016 Guillaume MARDON
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -37,27 +37,34 @@ char	*add_inf(char *val1, char *val2)
 {
   int	indexVal1;
   int	indexVal2;
+  int	index;
   char	*result;
   int	value;
   char	retained;
-  int 	maxLen;
-
-  maxLen = (my_strlen(val2) < my_strlen(val1) ? my_strlen(val1) : my_strlen(val2));
-  result = malloc(8 * (maxLen) + 1));
 
   indexVal1 = my_strlen(val1);
   indexVal2 = my_strlen(val2);
+  index = (indexVal2 < indexVal1 ? indexVal1 : indexVal2);
+  result = malloc(8 * ((index) + 1));
   retained = 0;
-  while (indexVal1 > 0 && indexVal2 > 0)
+  while (index > 0)
     {
       indexVal1--;
       indexVal2--;
-      value = (val1[indexVal1] - 48) + (val2[indexVal2] - 48) + retained;
+      index--;
+      if (indexVal1 < 0)
+	value = (val2[indexVal2] - 48) + retained;
+      else if (indexVal2 < 0)
+	value = (val1[indexVal1] - 48) + retained;
+      else
+	value = (val1[indexVal1] - 48) + (val2[indexVal2] - 48) + retained;
       if (value >= 10)
 	{
 	  retained = value / 10;
 	  value = value % 10;
 	}
+      else
+	retained = 0;
       result[index] = value + 48;
     }
   return (result);
