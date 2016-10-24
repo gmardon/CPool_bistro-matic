16495e252ce511447315b87a9b392e241e978c7e
Guillaume MARDON <guillaume.mardon@epitech.eu>
2016-10-24 10:23:56 +0200

    add somes

--- operators.c
+++ operators.c
@@ -5,7 +5,7 @@
 ** Login   <guillaume.mardon@epitech.eu>
 **
 ** Started on  Mon Oct 24 10:02:37 2016 Guillaume MARDON
-** Last update Mon Oct 24 10:19:25 2016 Guillaume MARDON
+** Last update Mon Oct 24 10:23:16 2016 Guillaume MARDON
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,10 +22,10 @@ char	*multiply_inf(char *val1, char *val2)
   indexVal1 = 0;
   while (indexVal1 < my_strlen(val1))
     {
-      printf("%d x %d", (val1[indexVal1] - 48), (val2[indexVal2] - 48));
       indexVal2 = 0;
       while (indexVal2 < my_strlen(val2))
 	{
+	  printf("%d x %d\n", (val1[indexVal1] - 48), (val2[indexVal2] - 48));
 	  indexVal2++;
 	}
       indexVal1++;
