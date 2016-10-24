99e7b9da6e844c11d8d455588194d57707b8341b
Guillaume MARDON <guillaume.mardon@epitech.eu>
2016-10-24 10:53:58 +0200

    add somes

--- operators.c
+++ operators.c
@@ -5,7 +5,7 @@
 ** Login   <guillaume.mardon@epitech.eu>
 **
 ** Started on  Mon Oct 24 10:02:37 2016 Guillaume MARDON
-** Last update Mon Oct 24 10:47:49 2016 Guillaume MARDON
+** Last update Mon Oct 24 10:53:18 2016 Guillaume MARDON
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -39,6 +39,17 @@ char	*add_inf(char *val1, char *val2)
   int 	indexVal2;
   char	*result;
 
-  result = malloc(sizeof(char*) * ((my_strlen(val2) < my_strlen(val1) ? my_strlen(val1) : my_strlen(val2)) + 1));
+  result = malloc(8 * ((my_strlen(val2) < my_strlen(val1) ? my_strlen(val1) : my_strlen(val2)) + 1));
 
+  indexVal1 = my_strlen(val1);
+  while (indexVal1 > 0)
+    {
+      indexVal1--;
+      indexVal2 = my_strlen(val2);
+      while (indexVal2 > 0)
+	{
+	  indexVal2--;
+	  printf("%d + %d = %d\n", (val1[indexVal1] - 48), (val2[indexVal2] - 48), (val1[indexVal1] - 48) + (val2[indexVal2] - 48));
+	}
+    }
 }
