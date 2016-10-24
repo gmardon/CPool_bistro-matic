4c759ed70eac87f30eecca6d2c579f0b971a5d7c
Guillaume MARDON <guillaume.mardon@epitech.eu>
2016-10-24 13:32:13 +0200

    add somes

--- operators.c
+++ operators.c
@@ -5,7 +5,7 @@
 ** Login   <guillaume.mardon@epitech.eu>
 **
 ** Started on  Mon Oct 24 10:02:37 2016 Guillaume MARDON
-** Last update Mon Oct 24 12:46:30 2016 Victor Le Dantec
+** Last update Mon Oct 24 13:31:53 2016 Guillaume MARDON
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -36,35 +36,27 @@ char	*add_inf(char *val1, char *val2)
 {
   int	indexVal1;
   int	indexVal2;
-  int	index;
   char	*result;
   int	value;
   char	retained;
 
   indexVal1 = my_strlen(val1);
   indexVal2 = my_strlen(val2);
-  index = (indexVal2 < indexVal1 ? indexVal1 : indexVal2);
-  result = malloc(8 * ((index) + 1));
+  result = malloc(8 * ((indexVal2 < indexVal1 ? indexVal1 : indexVal2) + 1));
   retained = 0;
-  while (index > 0)
+  while ((indexVal2 < indexVal1 ? indexVal1 : indexVal2) > 0)
     {
       indexVal1--;
       indexVal2--;
-      index--;
       if (indexVal1 < 0)
 	value = (val2[indexVal2] - 48) + retained;
       else if (indexVal2 < 0)
 	value = (val1[indexVal1] - 48) + retained;
       else
 	value = (val1[indexVal1] - 48) + (val2[indexVal2] - 48) + retained;
-      if (value >= 10)
-	{
-	  retained = value / 10;
-	  value = value % 10;
-	}
-      else
-	retained = 0;
-      result[index] = value + 48;
+      retained = (value >= 10 ? value / 10 : 0);
+      value = (value >= 10 ? value % 10 : value);
+      result[(indexVal2 < indexVal1 ? indexVal1 : indexVal2)] = value + 48;
     }
   return (result);
 }
