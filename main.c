fcee2def0847de1390c76e19a7a545be1081c4b1
Guillaume MARDON <guillaume.mardon@epitech.eu>
2016-10-24 10:07:10 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+** main.c for bistro-matic in /home/guillaume.mardon/delivery/CPool_bistro-matic/
+**
+** Made by Guillaume MARDON
+** Login   <guillaume.mardon@epitech.eu>
+**
+** Started on  Mon Oct 24 10:03:22 2016 Guillaume MARDON
+** Last update Mon Oct 24 10:05:06 2016 Guillaume MARDON
+*/
+#include "operators.c"
+
+int	main(int argc, char *argv)
+{
+  multiply_inf("38209", "38293");
+}
---
+++ operators.c
@@ -0,0 +1,13 @@
+/*
+** operators.c for bistro-matic in /home/guillaume.mardon/delivery/CPool_bistro-matic/
+**
+** Made by Guillaume MARDON
+** Login   <guillaume.mardon@epitech.eu>
+**
+** Started on  Mon Oct 24 10:02:37 2016 Guillaume MARDON
+** Last update Mon Oct 24 10:03:16 2016 Guillaume MARDON
+*/
+char	*multiply_inf(char *val1, char *val2)
+{
+
+}
