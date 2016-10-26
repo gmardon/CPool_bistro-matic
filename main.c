2134fd110eedc315012d666b2fc9fcbbf3d66402
Moana DUMORA <moana.dumora@epitech.eu>
2016-10-26 21:32:26 +0300

    aller cette fois cest la bonne

--- main.c
+++ main.c
@@ -5,11 +5,11 @@
 ** Login   <guillaume.mardon@epitech.eu>
 **
 ** Started on  Mon Oct 24 10:03:22 2016 Guillaume MARDON
-** Last update Wed Oct 26 09:26:20 2016 Guillaume MARDON
+** Last update Wed Oct 26 21:02:04 2016 Moana Dumora
 */
 #include "operators.c"
 
-int	main(int argc, char *argv)
+int	main(int argc, char **argv)
 {
-  printf("result: %s", add_inf("700", "300"));
+  add_inf(argv[1], argv[2]);
 }
--- operators.c
+++ operators.c
@@ -1,99 +1,41 @@
 /*
-** operators.c for bistro-matic in /home/guillaume.mardon/delivery/CPool_bistro-matic/
+** operators.c for bistro-matic in /home/moana.dumora/delivery/CPool_bistro-matic/
 **
-** Made by Guillaume MARDON
-** Login   <guillaume.mardon@epitech.eu>
+** Made by Moana DUMORA
+** Login   <moana.dumora@epitech.eu>
 **
-** Started on  Mon Oct 24 10:02:37 2016 Guillaume MARDON
-** Last update Wed Oct 26 13:11:41 2016 Guillaume MARDON
+** Started on  Mon Oct 24 10:02:37 2016 Moana Dumora
+** Last update Wed Oct 26 21:31:11 2016 Moana Dumora
 */
-#include <stdio.h>
+
 #include <stdlib.h>
-#include "utils.c"
+#include "set_index.c"
 
-char	*add_inf(char *val1, char *val2)
+void	add_inf(char *val1, char *val2)
 {
-  char	*result;
   int	index;
-  int	length;
-  int	value;
   int	retained;
-
-  if (my_strlen(val1) < my_strlen(val2))
-    val1 = equalise_numbers(val1, val2);
-  else if (my_strlen(val1) > my_strlen(val2))
-    val2 = equalise_numbers(val1, val2);
-  length = my_strlen(val1);
-  result = malloc(8 * (length) + 1);
-  index = length - 1;
-  retained = 0;
-  while (index >= 0)
-    {
-      value = (val1[index] - 48) + (val2[index] - 48) + retained;
-      retained = (value >= 10 ? value / 10 : 0);
-      value = (value >= 10 ? value % 10 : value);
-      result[index + 1] = value + 48;
-      index--;
-      if (index == -1 && retained != 0)
-	result[0] = retained + 48;
-    }
-  return result;
-}
-
-char	*minus_inf(char *val1, char *val2)
-{
   char	*result;
-  int	index;
-  int	length;
   int	value;
-  int	retained;
 
-  if (my_strlen(val1) < my_strlen(val2))
-    val1 = equalise_numbers(val1, val2);
-  else if (my_strlen(val1) > my_strlen(val2))
-    val2 = equalise_numbers(val1, val2);
-  length = my_strlen(val1);
-  result = malloc(8 * (length + 1));
-  index = length - 1;
-  retained = 0;
-  while (index > -1)
+  if (my_strlen(val1) > my_strlen(val2))
+    val2 = set_index(val1, val2);
+  else if (my_strlen(val2) > my_strlen(val1))
+    val1 = set_index(val1, val2);
+  index = my_strlen(val1);
+  result = malloc(8 * (index + 3));
+  if ((val1[0] + val2[0]) >= 10)
+    result[0] = '1';
+  while (index + 1 > 1)
     {
-      value = (val1[index] - 48) - ((val2[index] - 48) + retained);
-      if (value < 0)
-	  value = ((val1[index] - 48) + 10) - ((val2[index] - 48) + retained++);
-      value = (value >= 10 ? value % 10 : value);
+      value = (val2[index - 1] - 48) + (val1[index - 1] - 48);
+      if (value >= 10)
+	{
+	  value = value - 10;
+	  val2[index - 2] = val2[index - 2] + 1;
+	}
       result[index] = value + 48;
       index--;
     }
-  return result;
-}
-
-char	*multiply_inf(char *val1, char *val2)
-{
-  int	indexVal1;
-  int 	indexVal2;
-  char	*result;
-  int	value;
-  int	val2len;
-  int	val1len;
-
-  result = malloc(8 * (my_strlen(val1) + my_strlen(val2)));
-  str_set_zero(result, (my_strlen(val1) + my_strlen(val2)));
-  indexVal1 = 0;
-  val1len = my_strlen(val1);
-  while (indexVal1 < my_strlen(val1))
-    {
-      indexVal2 = val2len = my_strlen(val2);
-      while (indexVal2 > 0)
-	{
-	  indexVal2--;
-	  value = (val1[indexVal1] - 48)
-	      * my_power_rec(10, (val1len - indexVal1 - 1))
-	      * ((val2[indexVal2] - 48)
-	      * my_power_rec(10, (val2len - indexVal2 - 1)));
-	  result = add_inf(result, int_to_string(value));
-	}
-      indexVal1++;
-    }
-  return (result);
+  my_putstr(result);
 }
