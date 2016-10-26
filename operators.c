fba45f0f89d07935fad70ee2d1eb889dc778b130
Moana DUMORA <moana.dumora@epitech.eu>
2016-10-26 22:56:41 +0300

    fixed it

--- operators.c
+++ operators.c
@@ -5,11 +5,29 @@
 ** Login   <moana.dumora@epitech.eu>
 **
 ** Started on  Mon Oct 24 10:02:37 2016 Moana Dumora
-** Last update Wed Oct 26 21:31:11 2016 Moana Dumora
+** Last update Wed Oct 26 22:56:03 2016 Moana Dumora
 */
 
 #include <stdlib.h>
 #include "set_index.c"
+
+char	*couperdecaler(char *result)
+{
+  int	i;
+  char	*str;
+
+  str = result;
+  i = 0;
+  while (str[0] == '0')
+    {
+      while (result[i] != '\0')
+	{
+	  str[i] = result[i + 1];
+	  i++;
+	}
+    }
+  return (str);
+}
 
 void	add_inf(char *val1, char *val2)
 {
@@ -24,8 +42,7 @@ void	add_inf(char *val1, char *val2)
     val1 = set_index(val1, val2);
   index = my_strlen(val1);
   result = malloc(8 * (index + 3));
-  if ((val1[0] + val2[0]) >= 10)
-    result[0] = '1';
+  result[0] = ((val1[0] - 48 + val2[0] - 48) >= 10 ? '1' : '0');
   while (index + 1 > 1)
     {
       value = (val2[index - 1] - 48) + (val1[index - 1] - 48);
@@ -37,5 +54,6 @@ void	add_inf(char *val1, char *val2)
       result[index] = value + 48;
       index--;
     }
+  result = (couperdecaler(result));
   my_putstr(result);
 }
