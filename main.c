f568aaa61604b72d30cf20ff5eee2577200c7891
Moana DUMORA <moana.dumora@epitech.eu>
2016-10-26 23:28:35 +0300

    parceque le main cest important

--- main.c
+++ main.c
@@ -5,11 +5,14 @@
 ** Login   <guillaume.mardon@epitech.eu>
 **
 ** Started on  Mon Oct 24 10:03:22 2016 Guillaume MARDON
-** Last update Wed Oct 26 21:02:04 2016 Moana Dumora
+** Last update Wed Oct 26 23:24:16 2016 Moana Dumora
 */
 #include "operators.c"
 
 int	main(int argc, char **argv)
 {
-  add_inf(argv[1], argv[2]);
+  if (argc < 3 || argc > 3)
+    return (84);
+  else
+    add_inf(argv[1], argv[2]);
 }
