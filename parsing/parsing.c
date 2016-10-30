ea65ae4d0faf388af52eeebb868bb85ad98d4f2e
Moana <moana.dumora@epitech.eu>
2016-10-30 14:11:09 +0100

    aller

---
+++ parsing/parsing.c
@@ -0,0 +1,14 @@
+/*
+** parcing.c for bistro in /home/moana/Piscine/CPool_bistro-matic/parcing/
+**
+** Made by Moana Dumora
+** Login   <moana.dumora@epitech.eu@epitech.eu>
+**
+** Started on  Sun Oct 30 14:06:01 2016 Moana Dumora
+** Last update Sun Oct 30 14:06:06 2016 Moana Dumora
+*/
+
+char	*parser(char *str)
+{
+  
+}
