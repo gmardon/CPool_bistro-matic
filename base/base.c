1587758448d8bc2154757e8bdef3498493cfb8c3
Moana <moana.dumora@epitech.eu>
2016-11-01 18:59:42 +0100

    ca avance

--- base/base.c
+++ base/base.c
@@ -30,11 +30,29 @@ char	*etvalbase(char *base)
   return (val_base);
 }
 
-void	size_to_str(ssize_t total)
+char	*size_to_str(ssize_t total)
 {
+  ssize_t	tmp;
   ssize_t	i;
+  char		*str;
 
   i = 0;
+  tmp = total;
+  while (tmp > 0)
+    {
+      i++;
+      tmp = tmp / 10;
+    }
+  str = malloc(sizeof(char *) * (i + 1));
+  while (i > 0)
+    {
+      tmp = 0;
+      tmp = total % 10;
+      total = total / 10;
+      str[i - 1] = tmp + '0';
+      i--;
+    }
+  return (str);
 }
 
 char	*convert_to_ten(char *nbr, char *base)
@@ -44,6 +62,7 @@ char	*convert_to_ten(char *nbr, char *base)
   ssize_t	kindex;
   ssize_t	pow;
   char		*val_base;
+  char		*final;
 
   pow = my_strlen(nbr) - 1;
   val_base = malloc(sizeof(char *) * (my_strlen(base) + 1));
@@ -61,5 +80,80 @@ char	*convert_to_ten(char *nbr, char *base)
       else
 	kindex++;
     }
-  printf("%zu\n", total);
+  final = size_to_str(total);
+  return (final);
+}
+
+char    *my_baseptr(char *str, char **endptr, char *base)
+{
+  int   i;
+  char  *nbr;
+  int   k;
+  int   j;
+
+  i = k = j = 0;
+  nbr = malloc(2000);
+  while (*str != '\0')
+    {
+      if (*str != '*' && *str != '+' && *str != '-' && *str != '/' &&
+	  *str != '%' && *str != '(' && *str != ')')
+	{
+	  nbr[k] = *str;
+	  k++;
+	  if (str[1] == '*' && *str == '+' && *str == '-' && *str == '/' &&
+	      *str == '%' && *str == '(' && *str == ')')
+	    {
+	      str++;
+	      endptr[1] = str;
+	      return (convert_to_ten(nbr, base));
+	    }
+	}
+
+      str++;
+    }
+}
+
+char    *my_opptr(char *str, char **endptr)
+{
+  int   i;
+  char  *op;
+  int   k;
+  int   j;
+
+  i = k = j = 0;
+  op = malloc(sizeof(char *) * my_strlen(str));
+  while (*str != '\0')
+    {
+      if (*str == '-' || *str == '+' || *str == '*' || *str == '/'
+	  || *str == '%'  || *str == '(' || *str == ')')
+	{
+	  op[k] = *str;
+	  k++;
+	  if (str[1] <= '9' || str[1] >= '0')
+	    {
+	      str++;
+	      endptr[1] = str;
+	      return (op);
+	    }
+	}
+      str++;
+    }
+}
+
+char	*whole_convert(char *base, char *str, char **tab)
+{
+  char	*final;
+
+  final = malloc(sizeof(char *) * (my_strlen(str) + 1));
+  while (*str != '\0')
+    {
+      if (*str == '-' || *str == '+' || *str == '*' || *str == '/'
+	  || *str == '%'  || *str == '(' || *str == ')')
+	printf("%s\n", my_opptr(str, tab));
+      //final = my_strcat(final, my_opptr(str, tab));
+      else
+	printf("%s\n", my_baseptr(str, tab, base));
+      //final = my_strcat(final, my_baseptr(str, tab, base));
+    }
+  return (final);
 }
--- base/main.c
+++ base/main.c
@@ -1,6 +1,13 @@
+#include <stdio.h>
+
 char	*convert_to_ten(char *, char *);
+
+void	size_to_str(ssize_t);
+
+char	*whole_convert(char *, char *, char **);
 
 int	main(int ac, char **av)
 {
-  convert_to_ten(av[1], av[2]);
+  whole_convert(av[1], av[2], av);
+  printf("%s\n", whole_convert(av[1], av[2], av));
 }
