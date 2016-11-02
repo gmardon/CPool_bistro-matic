268f137b1e7489a7364c665d3556b28fcf71650d
Moana <moana.dumora@epitech.eu>
2016-11-02 15:21:28 +0100

    aller les bases ez

---
+++ base/ten_to_base.c
@@ -0,0 +1,66 @@
+/*
+** ten_to_base.c for bistro in /home/moana/Piscine/CPool_bistro-matic/base/
+**
+** Made by Moana Dumora
+** Login   <moana.dumora@epitech.eu@epitech.eu>
+**
+** Started on  Tue Nov  1 19:43:28 2016 Moana Dumora
+** Last update Tue Nov  1 22:22:43 2016 Moana Dumora
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t	my_strlen(char *);
+
+ssize_t	char_to_size(char *nbr)
+{
+  ssize_t	index;
+  ssize_t	total;
+
+  index = 0;
+  total = 0;
+  while (index <= my_strlen(nbr) - 1)
+    {
+      total = total * 10 + (nbr[index] - '0');
+      index++;
+    }
+  return (total);
+}
+
+ssize_t	get_result_size(ssize_t n, ssize_t b, ssize_t d)
+{
+  ssize_t	i;
+
+  i = 0;
+  while (n != 0)
+    {
+      d = n / b;
+      n = d;
+      i++;
+    }
+  return (i);
+}
+
+char	*ten_to_base(char *nbr, char *base)
+{
+  ssize_t	n;
+  char		*m;
+  ssize_t	d;
+  ssize_t	r;
+  ssize_t	b;
+  ssize_t	i;
+
+  b = my_strlen(base);
+  n = char_to_size(nbr);
+  i = get_result_size(n, b, d) - 1;
+  m = malloc(sizeof(char *) * (i + 1));
+  while (n != 0)
+    {
+      d = n / b;
+      r = n % b;
+      n = d;
+      m[i] = base[r];
+      i--;
+    }
+}
