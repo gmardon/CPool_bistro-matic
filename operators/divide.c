6bbdc7878a64d0198562c9bfcbbd7941577f769d
Guillaume MARDON <guillaume.mardon@epitech.eu>
2016-11-05 17:50:12 +0100

    divide :)

--- operators/divide.c
+++ operators/divide.c
@@ -5,7 +5,7 @@
 ** Login   <guillaume.mardon@epitech.eu@epitech.eu>
 **
 ** Started on  Sun Oct 30 14:32:57 2016 Guillaume MARDON
-** Last update Sat Nov  5 17:29:08 2016 Guillaume MARDON
+** Last update Sat Nov  5 17:49:43 2016 Guillaume MARDON
 */
 
 #include "divide.h"
@@ -29,46 +29,18 @@ char	*divide(char *val1, char *val2)
   result_index = 0;
   while ((diff + 1) != 0)
     {
-      printf("begin with target: %s\n", target);
-      printf("diff: %d", diff);
       count = 0;
       value = malloc(sizeof(char *) * (diff + 1));
       str_set_zero(value, (diff + 1));
       put_chars_with_offset(value, val2, diff);
-			printf("value: %s\n", value);
-
       while (is_greater("0", target) != 1)
 				{
 						count++;
 	  				target = minus(target, value);
-	  				printf("target: %s\n", target);
 	  		}
       target = add(target, value);
-
-				printf("index: %d, count: %d\n", result_index, count - 1);
-
-
       result[result_index++] = (count - 1) + 48;
       diff--;
-      /*
-      count = 0;
-      temp = malloc(sizeof(char *) * diff);
-
-      nbr = put_chars_with_offset(nbr, val2, my_strlen(val2) - diff);
-      printf("v: %s, m: %d, nbr: %s\n", val2, my_strlen(val2) - diff, nbr);
-    //  put_chars_with_offset(temp, val2, my_strlen(val2) - diff);
-			printf("nbr: '%s'\n", nbr);
-    //  return "0";
-      while (is_greater(temp, val1) == -1)
-				{
-						count++;
-	  				printf("nbr: %s\n", nbr);
-	  				temp = add(temp, nbr);
-						printf("temp: %s\n", temp);
-				}*/
-
-      //result = add(result, size_to_str(count));
     }
-  	printf("result: '%s'\n", result);
 		return (result);
 }
