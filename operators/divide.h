d2452d8c03e96adc4ab396c0d3ada5a7b4449191
Guillaume MARDON <guillaume.mardon@epitech.eu>
2016-11-05 19:17:28 +0100

    yeah

--- operators/divide.h
+++ operators/divide.h
@@ -1,15 +1,17 @@
 /*
 ** divide.h for divide in /home/victor/Piscine/CPool_bistro-matic
-** 
+**
 ** Made by Victor Le Dantec
 ** Login   <victor.le-dantec@epitech.net>
-** 
+**
 ** Started on  Fri Nov  4 17:01:04 2016 Victor Le Dantec
-** Last update Sat Nov  5 18:47:56 2016 Victor LE DANTEC
+** Last update Sat Nov  5 19:16:10 2016 Guillaume MARDON
 */
 
 #include <stdlib.h>
 
+char	*divide_handle_negative(char *, char *);
+char	*divide(char *, char *);
 void	*put_chars_with_offset(char *, char *, int);
 int	is_greater(char *, char *);
 size_t	my_strlen(char *);
