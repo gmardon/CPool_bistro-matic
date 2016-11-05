9d0b45b3575170617e8148fe27f7ae184e0b4139
Victor Le Dantec <victor.le-dantec@epitech.eu>
2016-11-05 18:24:15 +0100

    AS TU VU

--- operators/modulo.c
+++ operators/modulo.c
@@ -5,7 +5,7 @@
 ** Login   <moana.dumora@epitech.eu@epitech.eu>
 **
 ** Started on  Sat Nov  5 09:40:33 2016 Moana Dumora
-** Last update Sat Nov  5 15:50:55 2016 Victor LE DANTEC
+** Last update Sat Nov  5 18:16:03 2016 Victor LE DANTEC
 */
 
 #include <stdlib.h>
@@ -14,7 +14,5 @@ int	my_strcmp(char *, char *);
 
 char	*modulo(char *nbr, char *modulus)
 {
-  while (my_strcmp(nbr, modulus) != -1)
-      nbr = minus(nbr, modulus);
-  return (nbr);
+  return (minus(nbr, multiply(my_strdup(modulus), divide(nbr, modulus))));
 }
