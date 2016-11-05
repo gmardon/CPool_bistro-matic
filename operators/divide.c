1397d22f9a88f1ccef1cf5dd9ac200ae4b06fdb3
Guillaume MARDON <guillaume.mardon@epitech.eu>
2016-11-05 18:13:38 +0100

    fix negative number

--- operators/divide.c
+++ operators/divide.c
@@ -5,10 +5,27 @@
 ** Login   <guillaume.mardon@epitech.eu@epitech.eu>
 **
 ** Started on  Sun Oct 30 14:32:57 2016 Guillaume MARDON
-** Last update Sat Nov  5 17:49:43 2016 Guillaume MARDON
+** Last update Sat Nov  5 18:13:11 2016 Guillaume MARDON
 */
 
 #include "divide.h"
+
+char	*divide_handle_negative(char *val1, char *val2)
+{
+  val1 = clean_negation(val1);
+  val2 = clean_negation(val2);
+  if ((is_neg(val1) || is_neg(val2)) && !(is_neg(val1) && is_neg(val2)))
+    {
+      if (is_neg(val1) && !is_neg(val2))
+	  				return (set_negative(divide(val1 + 1, val2)));
+      else if (!is_neg(val1) && is_neg(val2))
+	  				return (set_negative(divide(val1, val2 + 1)));
+    }
+	else if (is_neg(val1) && is_neg(val2))
+	 {
+	 		return (divide(val1 + 1, val2 + 1));
+	 }
+}
 
 char	*divide(char *val1, char *val2)
 {
@@ -22,7 +39,8 @@ char	*divide(char *val1, char *val2)
 
   if (my_strlen(val1) < my_strlen(val2) || is_greater(val2, val1) == 1)
     return ("0");
-
+  if (is_neg(val1) || is_neg(val2))
+    return (divide_handle_negative(val1, val2));
   diff = my_strlen(val1) - my_strlen(val2);
   result = malloc(sizeof(char *) * my_strlen(val1));
   target = my_strdup(val1);
