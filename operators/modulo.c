d85163c2bbdcb090bf6df7797ae74fab8083b009
Moana <moana.dumora@epitech.eu>
2016-11-05 09:50:30 +0100

    le modulo ez

---
+++ operators/modulo.c
@@ -0,0 +1,9 @@
+/*
+** modulo.c for bistro in /home/moana/Piscine/CPool_bistro-matic/operators/
+**
+** Made by Moana Dumora
+** Login   <moana.dumora@epitech.eu@epitech.eu>
+**
+** Started on  Sat Nov  5 09:40:33 2016 Moana Dumora
+** Last update Sat Nov  5 09:40:38 2016 Moana Dumora
+*/
