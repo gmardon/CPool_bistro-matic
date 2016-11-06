b415bf9cc1dec3ff3c87ef2d1e0eeaf622b1b9be
Moana <moana.dumora@epitech.eu>
2016-11-06 20:54:10 +0100

    victor doit sexcuser

--- operators/divide.c
+++ operators/divide.c
@@ -8,6 +8,7 @@
 ** Last update Sat Nov  5 19:12:28 2016 Guillaume MARDON
 */
 
+#include "winline.h"
 #include "divide.h"
 
 char	*divide_handle_negative(char *val1, char *val2)
@@ -17,48 +18,40 @@ char	*divide_handle_negative(char *val1, char *val2)
   if ((is_neg(val1) || is_neg(val2)) && !(is_neg(val1) && is_neg(val2)))
     {
       if (is_neg(val1) && !is_neg(val2))
-	  				return (set_negative(divide(val1 + 1, val2)));
+	return (set_negative(divide(val1 + 1, val2)));
       else if (!is_neg(val1) && is_neg(val2))
-	  				return (set_negative(divide(val1, val2 + 1)));
+	return (set_negative(divide(val1, val2 + 1)));
     }
-	else if (is_neg(val1) && is_neg(val2))
-	 {
-	 		return (divide(val1 + 1, val2 + 1));
-	 }
+  else if (is_neg(val1) && is_neg(val2))
+    return (divide(val1 + 1, val2 + 1));
 }
 
 char	*divide(char *val1, char *val2)
 {
-  size_t diff;
-  char *target;
-  char *value;
-  char *number;
-  char *result;
-  int count;
-  int result_index;
+  t_guigui	np;
 
   if (my_strlen(val1) < my_strlen(val2) || is_greater(val2, val1) == 1)
     return ("0");
   if (is_neg(val1) || is_neg(val2))
     return (divide_handle_negative(val1, val2));
-  diff = my_strlen(val1) - my_strlen(val2);
-  result = malloc(sizeof(char *) * my_strlen(val1));
-  target = my_strdup(val1);
-  result_index = 0;
-  while ((diff + 1) != 0)
+  np.diff = my_strlen(val1) - my_strlen(val2);
+  np.result = malloc(sizeof(char *) * my_strlen(val1));
+  np.target = my_strdup(val1);
+  np.result_index = 0;
+  while ((np.diff + 1) != 0)
     {
-      count = 0;
-      value = malloc(sizeof(char *) * (diff + 1));
-      str_set_zero(value, (diff + 1));
-      put_chars_with_offset(value, val2, diff);
-      while (is_greater("0", target) != 1)
-				{
-						count++;
-	  				target = minus(target, value);
-	  		}
-      target = add(target, value);
-      result[result_index++] = (count - 1) + 48;
-      diff--;
+      np.count = 0;
+      np.value = malloc(sizeof(char *) * (np.diff + 1));
+      str_set_zero(np.value, (np.diff + 1));
+      put_chars_with_offset(np.value, val2, np.diff);
+      while (is_greater("0", np.target) != 1)
+	{
+	  np.count++;
+	  np.target = minus(np.target, np.value);
+	}
+      np.target = add(np.target, np.value);
+      np.result[np.result_index++] = (np.count - 1) + 48;
+      np.diff--;
     }
-  return (remove_zeros(result));
+  return (remove_zeros(np.result));
 }
---
+++ operators/winline.h
@@ -0,0 +1,17 @@
+#ifndef	WINLINES_H_
+#define WINLINES_H_
+
+#include <stdlib.h>
+
+typedef struct	s_poubelle
+{
+  size_t diff;
+  char *target;
+  char *value;
+  char *number;
+  char *result;
+  int count;
+  int result_index;
+}t_guigui;
+
+#endif	/* WINLINES_H_ */
